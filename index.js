5ff1759927334f04ad2a50f6a38f5116ec101f5c
joe lazarz <jlazarz87@gmail.com>
2019-08-15 14:20:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+class Cat {
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+        this.speak = function(){
+            return `${name} says meow!`
+        }
+    }
+}
+
+class Dog {
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+        this.speak = function(){
+            return `${name} says woof!`
+        }
+    }
+}
+
+class Bird {
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+        this.speak = function(){
+            if (this.sex == 'male'){
+                return `It's me! ${name}, the parrot!`
+            } else {
+                return `${name} says squawk!`
+            }
+        }
+    }
+}
